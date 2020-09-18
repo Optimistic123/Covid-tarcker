2d809061dc2355b40c2dde6e7208528aef12ddae
Manish kumar <manishkj441@gmail.com>
2020-09-18 23:59:24 +0530

    Tables and card is structured

---
+++ src/Table.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Table.css";
+//import numeral from "numeral";
+
+function Table({ countries }) {
+  return (
+    <div className="table">
+      {countries.map((country) => (
+        <tr>
+          <td>{country.country}</td>
+          <td>
+            <strong>{country.cases}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
+}
+
+export default Table;
